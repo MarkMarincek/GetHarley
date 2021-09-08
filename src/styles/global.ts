d4e381ad2e9339d8ff7e1b09719fed57e6d3eb7e
Mark Marincek <mark.marincek@gmail.com>
2021-09-08 23:36:39 +0200

    Added global styles, replaced CRA app content with the routing system.

---
+++ src/styles/global.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components/macro';
+import reset from 'styled-reset';
+
+export default createGlobalStyle`
+  ${reset};
+  
+  html {
+    overflow: hidden;
+      height: 100%;
+  }
+  * {
+    box-sizing: border-box
+  }
+  body {
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+    height: 100%;
+    overflow: auto;
+  }
+`;
