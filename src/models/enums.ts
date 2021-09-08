ccf25c3f918c67daf2584986001dffd41932cea0
Mark Marincek <mark.marincek@gmail.com>
2021-09-08 21:42:31 +0200

    Added interfaces.

---
+++ src/models/enums.ts
@@ -0,0 +1,14 @@
+export enum PersonTitle {
+  Mr = 'mr',
+  Ms = 'ms',
+  Mrs = 'mrs',
+  Miss = 'miss',
+  Dr = 'dr',
+  None = '',
+}
+
+export enum Gender {
+  Male = 'male',
+  Female = 'female',
+  Other = 'other',
+}
---
+++ src/models/interfaces.ts
@@ -0,0 +1,45 @@
+import { Gender, PersonTitle } from './enums';
+
+export interface PaginationConfig {
+  page: number;
+  limit?: number;
+}
+
+export interface ApiResponse<T> {
+  data: T;
+  status: number;
+  statusText: string;
+}
+
+export interface ApiList<T> {
+  data: T[];
+  total: number;
+  page: number;
+  limit: number;
+}
+
+export interface UserPreview {
+  id: string;
+  title: PersonTitle;
+  firstName: string;
+  lastName: string;
+  picture: string;
+}
+
+export interface UserFull extends UserPreview {
+  gender: Gender;
+  email: string;
+  dateOfBirth: string; // ISO Date - value: 1/1/1900 - now
+  registerDate: string;
+  phone: string; // Any format
+  picture: string;
+  location: Location;
+}
+
+export interface Location {
+  street: string;
+  city: string;
+  state: string;
+  country: string;
+  timezone: string; // Valid timezone value ex. +7:00, -1:00
+}
