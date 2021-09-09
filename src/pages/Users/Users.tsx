da70b8820e774cf8d6f8a05ef99608e8d38adc9c
Mark Marincek <mark.marincek@gmail.com>
2021-09-09 03:52:53 +0200

    Updated the Users page. Added initial data loading, replaced placeholder cards with UserCard components.

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -1,17 +1,25 @@
 import GridWrapper from 'components/GridWrapper';
 import StyledCard from 'components/StyledCard';
-import React from 'react';
+import UserCard from 'components/UserCard';
+import useApiHook from 'hooks/useApi.hook';
+import React, { useEffect } from 'react';
+import { getUsers } from 'utils/api';
 
 export default function Users() {
+  const [data, requestUsers] = useApiHook(() => getUsers());
+
+  useEffect(() => {
+    requestUsers();
+  }, []);
+
   return (
     <>
       <h1>Users list</h1>
       <div>
         <GridWrapper>
-          <StyledCard>hello world</StyledCard>
-          <StyledCard>hello world</StyledCard>
-          <StyledCard>hello world</StyledCard>
-          <StyledCard>hello world</StyledCard>
+          {data.response?.data.data.map((user) => (
+            <UserCard user={user} key={user.id} />
+          ))}
         </GridWrapper>
       </div>
     </>
