9ccc976d16633d66f7ff53904879ba46d6a46688
Mark Marincek <mark.marincek@gmail.com>
2021-09-10 08:17:51 +0200

    Updated the useApi hook to be more usable.

--- src/hooks/useApi.hook.ts
+++ src/hooks/useApi.hook.ts
@@ -1,27 +1,32 @@
 import axios, { Canceler } from 'axios';
-import { useRef, useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 
 interface ApiHookState<T> {
   loading: boolean;
   response?: T;
   error?: unknown;
 }
-export default function useApiHook<T>(fun: () => Promise<T>) {
+export default function useApiHook<K, T>(fun: (args?: K) => Promise<T>) {
   const [state, setState] = useState<ApiHookState<T>>({ loading: false });
   const previousRequestCancelRef = useRef<Canceler | undefined>(undefined);
-  async function callApi() {
-    // Cancels the previous request if that has not finished yet
-    if (previousRequestCancelRef.current) previousRequestCancelRef.current();
-    setState((s) => ({ ...s, loading: true }));
 
-    try {
-      const promise = fun();
-      previousRequestCancelRef.current = axios.CancelToken.source().cancel;
-      const response = await promise;
-      setState((s) => ({ ...s, loading: false, response }));
-    } catch (error) {
-      setState((s) => ({ ...s, loading: false, error }));
-    }
-  }
-  return [state, callApi] as [ApiHookState<T>, () => void];
+  const callApi = useCallback(
+    async (args: K) => {
+      // Cancels the previous request if that has not finished yet
+      if (previousRequestCancelRef.current) previousRequestCancelRef.current();
+      setState((s) => ({ ...s, loading: true }));
+
+      try {
+        const promise = fun(args);
+        previousRequestCancelRef.current = axios.CancelToken.source().cancel;
+        const response = await promise;
+        setState((s) => ({ ...s, loading: false, response }));
+      } catch (error) {
+        setState((s) => ({ ...s, loading: false, error }));
+      }
+    },
+    [fun]
+  );
+
+  return [state, callApi] as [ApiHookState<T>, (args?: K) => void];
 }
